06e3fc9536c123e2575c15d7f5f2f23368b76d91
Amit Thakkar <vigildbest@gmail.com>
2015-08-23 16:58:54 -0700

    using ng-for with model.

--- client/app.ts
+++ client/app.ts
@@ -1,11 +1,29 @@
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
 
 @Component({
     selector: 'app'
 })
 @View({
-    template: "<p>Hello World!</p>"
+    templateUrl: 'client/index.ng.html',
+    directives: [NgFor]
 })
-class Socially {}
+class Socially {
+    constructor() {
+        this.parties = [
+            {
+                'name': 'Dubstep-Free Zone',
+                'description': 'Can we please just for an evening not listen to dubstep.'
+            },
+            {
+                'name': 'All dubstep all the time',
+                'description': 'Get it on!'
+            },
+            {
+                'name': 'Savage lounging',
+                'description': 'Leisure suit required. And only fiercest manners.'
+            }
+        ];
+    }
+}
 
 bootstrap(Socially);
