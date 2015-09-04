fe18d9a726df87d1eaf68dfad18e87603d51248b
Amit Thakkar <vigildbest@gmail.com>
2015-09-04 12:03:01 -0700

    Adding form component.

--- client/app.ts
+++ client/app.ts
@@ -2,19 +2,23 @@
  * Created by Amit Thakkar on 8/23/15.
  */
 import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
+import {PartiesForm} from 'client/parties-form/parties-form';
 
 @Component({
     selector: 'app'
 })
 @View({
     templateUrl: 'client/index.ng.html',
-    directives: [NgFor]
+    directives: [NgFor, PartiesForm]
 })
 class Socially {
     constructor() {
         Tracker.autorun(zone.bind(() => {
             this.parties = Parties.find().fetch();
         }));
+    }
+    remove(party) {
+        Parties.remove(party._id);
     }
 }
 
